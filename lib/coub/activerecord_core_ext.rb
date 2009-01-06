6476353db686891e5181b6eb8f5251c2342c8630
Jens Benecke <jens-github@spamfreemail.de>
2009-01-06 14:22:26 +0100

    Removed useless comments, translated comments

--- lib/coub/activerecord_core_ext.rb
+++ lib/coub/activerecord_core_ext.rb
@@ -1,33 +1,5 @@
 # http://www.intridea.com/2008/2/19/activerecord-base-create_or_update-on-steroids-2
-
-# class << ActiveRecord::Base
-#   def create_or_update(options = {})
-#     self.create_or_update_by(:id, options)
-#   end
-# 
-#   def create_or_update_by(field, options = {})
-#     find_value = options.delete(field)
-#     record = find(:first, :conditions => {field => find_value}) || self.new
-#     record.send field.to_s + "=", find_value
-#     record.attributes = options
-#     record.save!
-#     record
-#   end
-# 
-#   def method_missing_with_create_or_update(method_name, *args)
-#     if match = method_name.to_s.match(/create_or_update_by_([a-z0-9_]+)/)
-#       field = match[1].to_sym
-#       create_or_update_by(field,*args)
-#     else
-#       method_missing_without_create_or_update(method_name, *args)
-#     end
-#   end
-# 
-#   alias_method_chain :method_missing, :create_or_update
-# end
-
-
-# erweitert: http://github.com/jeremymcanally/docbox/tree/57215361c3a585a9608bee25cd6e4659c780ff17/lib/core_ext.rb
+# http://github.com/jeremymcanally/docbox/tree/57215361c3a585a9608bee25cd6e4659c780ff17/lib/core_ext.rb
 class << ActiveRecord::Base
   def create_or_update(options = {})
     self.create_or_update_by(:id, options)
